cca15af4a2adad76473bb1221e5f9f08952b9eaa
David-Melik <davidmelik05@gmail.com>
2026-04-22 17:46:08 +0300

    Item collider fix, collect when the collider is the player and not other entities

--- Assets/Scripts/itemController.cs
+++ Assets/Scripts/itemController.cs
@@ -5,7 +5,9 @@ public class ItemController : MonoBehaviour {
   public int itemId;
 
   private void OnTriggerEnter2D(Collider2D other) {
-    InventoryController.Instance.AddItem(itemId);
-    Destroy(gameObject); // optional: remove item from world
+    if (other.CompareTag("Player")) {
+      InventoryController.Instance.AddItem(itemId);
+      Destroy(gameObject); // optional: remove item from world
+    }
   }
 }
