57f272d10fb457a3d176cbffbfe1e62115bd9e33
David-Melik <davidmelik05@gmail.com>
2026-05-14 19:31:10 +0300

    setup signal receiver and emitter

---
+++ Assets/Scripts/level/entryCinematicDiscussion.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+using UnityEngine.Playables;
+using System.Collections;
+
+public class entryCinematicDiscussion : MonoBehaviour {
+  public PlayableDirector director;
+
+  public void SpawnEnemy() { Debug.Log("Enemy Spawned!"); }
+}
